7ec61bf79f658234dec231fa72b7e0e9e1c182b3
nidawahid28 <wahidnida761@gmail.com>
2024-06-28 02:54:36 +0500

    This is my first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import {differenceInSeconds} from "date-fns";
+
+const res = await inquirer.prompt(
+    [
+        {
+            name: "userInput",
+            type: "number",
+            message: "Please enter the amount of second",
+            validate: (input) => {
+                if(isNaN(input)){
+                    return "Please enter valid number"
+                }else if(input > 60){
+                    return "Seconds must be in 60"
+                }else{
+                    return true;
+                }
+            }
+        }
+    ]
+);
+
+let input = res.userInput
+
+function startTime(val:number){
+    const inTime = new Date().setSeconds(new Date().getSeconds() + val+2);
+    const inertervalTime = new Date(inTime);
+    setInterval((()=>{
+        const currTime = new Date()
+        const timeDiff = differenceInSeconds(inertervalTime, currTime);
+
+        if(timeDiff <= 0){
+            console.log("Timer has Expired");
+            process.exit()
+        }
+        const min = Math.floor((timeDiff%(3600*24))/3600)
+        const sec = Math.floor(timeDiff%60)
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }),1000)
+}
+startTime(input);
+
+
+
+
+
+
